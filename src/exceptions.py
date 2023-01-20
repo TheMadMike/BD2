ef4efe9e2b87ca83b3ffa3c48fa9c4fb297fa7a5
TheMadMike <m.gibas2407@gmail.com>
2023-01-20 13:50:51 +0100

    Add TeacherNotFound exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,3 +2,7 @@ class StudentNotFound(Exception):
     def __init__(self, pesel):
         super().__init__(f"Unable to find student with PESEL: {pesel}")
         self.pesel = pesel
+
+class TeacherNotFound(Exception):
+    def __init__(self, pesel):
+        super().__init__(f"Unable to find teacher with PESEL: {pesel}")
